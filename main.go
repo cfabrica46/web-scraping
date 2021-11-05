8bb9ff8f843cf37074c97c3c3fa645a974474eee
Cesar <c.caycho46@gmail.com>
2021-11-05 01:36:55 -0500

    completed

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	resp, err := http.Get("https://golang.org/dl/")
+	/* resp, err := http.Get("https://golang.org/dl/")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -20,7 +20,7 @@ func main() {
 	r, err := regexp.Compile(`\/dl\/go([0-9\.]+)\.linux-amd64\.tar\.gz`)
 	if err != nil {
 		log.Fatal(err)
-	}
+	} */
 
 	//print all coincidences
 	/* for scanner.Scan() {
@@ -31,7 +31,7 @@ func main() {
 	} */
 
 	//get last version
-	var value string
+	/* var value string
 	for scanner.Scan() {
 		text := r.FindString(scanner.Text())
 		if len(text) != 0 {
@@ -40,5 +40,26 @@ func main() {
 		}
 	}
 	url := fmt.Sprintf("golang.org%s", value)
-	fmt.Printf("The url of the last go version is: %s\n", url)
+	fmt.Printf("The url of the last go version is: %s\n", url) */
+
+	resp, err := http.Get("https://www.hectormainar.com/h1.php")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	r, err := regexp.Compile(`(?s)<h1>.+</h1>`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scanner := bufio.NewScanner(resp.Body)
+
+	for scanner.Scan() {
+		text := r.FindString(scanner.Text())
+		if len(text) != 0 {
+			fmt.Println(text)
+			break
+		}
+	}
 }
