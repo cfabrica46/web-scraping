384885f489bf80b10fe0f685d3b03d388b13da12
Cesar <c.caycho46@gmail.com>
2021-11-05 22:34:06 -0500

    completed

--- main.go
+++ main.go
@@ -1,66 +1,66 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
-	"regexp"
+	"strings"
+
+	"golang.org/x/net/html"
 )
 
+//regex method
+/* exampleText := `<h2>holasss ssg sdfa</h2>
+
+<p><h1 class='hola'>hola q talk</h1></p>`
+reader := strings.NewReader(exampleText)
+
+_, err = regexp.Compile("(?s)<h1.*>.+</h1>")
+if err != nil {
+	log.Fatal(err)
+} */
+
 func main() {
-	/* resp, err := http.Get("https://golang.org/dl/")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	scanner := bufio.NewScanner(resp.Body)
-
-	r, err := regexp.Compile(`\/dl\/go([0-9\.]+)\.linux-amd64\.tar\.gz`)
-	if err != nil {
-		log.Fatal(err)
-	} */
-
-	//print all coincidences
-	/* for scanner.Scan() {
-		text := r.FindString(scanner.Text())
-		if len(text) != 0 {
-			fmt.Println(text)
-		}
-	} */
-
-	//get last version
-	/* var value string
-	for scanner.Scan() {
-		text := r.FindString(scanner.Text())
-		if len(text) != 0 {
-			value = text
-			break
-		}
-	}
-	url := fmt.Sprintf("golang.org%s", value)
-
-	fmt.Printf("The url of the last go version is: %s\n", url) */
-
 	resp, err := http.Get("https://www.hectormainar.com/h1.php")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
 
-	r, err := regexp.Compile("(?s)<h1>.+</h1>")
-	if err != nil {
-		log.Fatal(err)
-	}
+	tokenizer := html.NewTokenizer(resp.Body)
+	var enter bool
 
-	scanner := bufio.NewScanner(resp.Body)
+	for {
+		tt := tokenizer.Next()
+		token := tokenizer.Token()
 
-	for scanner.Scan() {
-		text := r.FindString(scanner.Text())
-		if len(text) != 0 {
-			fmt.Println(text)
+		err := tokenizer.Err()
+		if err == io.EOF {
 			break
+		}
+
+		switch tt {
+		case html.ErrorToken:
+			log.Fatal(err)
+
+		case html.StartTagToken, html.SelfClosingTagToken:
+			enter = false
+
+			tag := token.Data
+			if tag == "h1" {
+				enter = true
+			}
+		case html.TextToken:
+			if enter {
+				data := strings.TrimSpace(token.Data)
+
+				if len(data) > 0 {
+					fmt.Println("First h1:")
+					fmt.Println(data)
+					return
+				}
+			}
 		}
 	}
 }
